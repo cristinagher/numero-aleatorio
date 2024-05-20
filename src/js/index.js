2f9856a3fe1ce67a483de58bd44e9e4b855747f0
Cristina Gámez <163881246+cristinagher@users.noreply.github.com>
2024-05-20 18:53:32 +0200

    update

--- src/js/index.js
+++ src/js/index.js
@@ -146,3 +146,96 @@ function userName(name, surname) {
 }
 
 userName("Cristina", "Hernández");
+
+//Crea una función que reciba un array de 5 números, imprime por consola una posición aleatoria del array.
+
+const getRandomNumber = (numbers) => {
+  const random = Math.floor(Math.random() * numbers.length);
+
+  console.log(numbers[random]);
+};
+getRandomNumber([1, 2, 3, 4, 5]);
+
+//Crea una función que reciba un array con 3 números. La función deberá imprimir por consola.
+
+// "La suma de todos los números es:[suma]"
+// "La media de todos los números es:[media]"
+// "El mayor es [mayor] y el menor es [menor]"
+
+const getOperations = (numbers) => {
+  const suma = numbers[0] + numbers[1] + numbers[2];
+  console.log(`La suma de todos los números es ${suma}`);
+  const media = suma / numbers.length;
+  console.log(`La media de todos los númberos es ${media}`);
+};
+
+//Crea una función que reciba un array con 5 números del 0 al 10 (a tu elección). Dentro de la función genera un número aleario entre 0 y 10. La función deberá decir si el array contiene ese número y en qué posición está o si no lo contiene.
+
+const aleatoryNumbers = (numbers) => {
+  const getNumberRandom = Math.floor(Math.random() * 11);
+  if (numbers.includes(getNumberRandom)) {
+    console.log(
+      `El array contiene ${getNumberRandom} y se encuentra en ${numbers.indexOf(
+        getNumberRandom
+      )}`
+    );
+  } else {
+    console.log("El array no contiene este número");
+  }
+};
+aleatoryNumbers([2, 7, 8, 3, 5]);
+//crea una función que reciba un array vacío y lo devuelva con 3 números aleatorios entre 0 y 100.
+
+const emptyArray = (numbers) => {
+  const getRandomNumberA = Math.floor(Math.random() * 101);
+  const getRandomNumberB = Math.floor(Math.random() * 101);
+  const getRandomNumberC = Math.floor(Math.random() * 101);
+
+  if (numbers.length === 0) {
+    numbers.push(getRandomNumberA, getRandomNumberB, getRandomNumberC);
+    console.log(numbers);
+  } else {
+    console.log("mal");
+  }
+};
+
+emptyArray([]);
+
+//Crea una función que reciba un array de 5 números. Dentro de esa función crea dos arrays vacíos llamados even (pares) y odd (impares), después multiplica cada uno de los números del array recibido por un número aleatorio entre 1 y 10, si el resultado es par, guárdalo en el array de pares, si es impar, en el array de impares, al final, imprime los 3 arrays por consola
+const queHartura = (numbers) => {
+  const even = [];
+  const odd = [];
+  const getRandomNumber = Math.floor(Math.random() * 11);
+  const positionA = numbers[0] * getRandomNumber;
+  const positionB = numbers[1] * getRandomNumber;
+  const positionC = numbers[2] * getRandomNumber;
+  const positionD = numbers[3] * getRandomNumber;
+  const positionE = numbers[4] * getRandomNumber;
+  if (positionA / 2 === 0) {
+    even.push(positionA);
+  } else if (positionB / 2 === 0) {
+    even.push(positionB);
+  } else if (positionC / 2 === 0) {
+    even.push(positionC);
+  } else if (positionD / 2 === 0) {
+    even.push(positionD);
+  } else if (positionE / 2 === 0) {
+    even.push(positionE);
+  }
+  if (positionA / 2 > 0) {
+    odd.push(positionA);
+  }
+  if (positionB / 2 > 0) {
+    odd.push(positionB);
+  }
+  if (positionC / 2 > 0) {
+    odd.push(positionC);
+  }
+  if (positionD / 2 > 0) {
+    odd.push(positionD);
+  }
+  if (positionE / 2 > 0) {
+    odd.push(positionE);
+  }
+};
+queHartura([1, 2, 3, 4, 5]);
